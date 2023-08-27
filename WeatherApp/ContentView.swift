6c6a62b77ea6e00b5c66c610ef6bc0f8227bfc2a
Yusuf Karan <karan.yusuf.20@gmail.com>
2023-08-27 15:47:40 +0300

    Created Project

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Yusuf Karan on 27.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yusuf Karan on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
